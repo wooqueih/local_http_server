11605e94dc93e3496c9fd35291ecc7db2e8d4d1f
Michael Pogrebnjak <michael@pogrebnjak.de>
2023-02-27 16:52:03 +0100

    1.0.1 now with a macro

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+macro_rules! get_file_not_found_html {
+    () => {
+        fs::read("./404.html").unwrap()
+    }
+} 
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -13,6 +19,7 @@ fn main() {
         handle_connection(stream);
     }
 }
+
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
@@ -39,14 +46,12 @@ fn handle_connection(mut stream: TcpStream) {
             Err(_e) => {
                 status_line = "HTTP/1.1 404 NOT FOUND";
                 content_type = "text/html; charset=UTF-8";
-                get_file_not_found_html().to_string().as_bytes().to_vec()
+                get_file_not_found_html!()
             },
         };
 
         let length = contents.len();
-
-        //let response = format!("{status_line}\r\nContent-Type: {content_type} Content-Length: {length}\r\n\r\n{contents}");
-        //stream.write_all(response.as_bytes()).unwrap();
+        
         let response = format!("{status_line}\r\nContent-Type: {content_type}\nContent-Length: {length}\r\n\r\n");
         let response = response.as_bytes();
         let mut response = response.to_vec();
@@ -57,10 +62,6 @@ fn handle_connection(mut stream: TcpStream) {
 
         let _ = stream.write_all(response.as_slice());
     }
-}
-
-fn get_file_not_found_html() -> &'static str{
-    return "<h1>Error 404 : file not found</h1>";
 }
 
 fn get_content_type(path: &std::path::Path) -> &str {
